ab060759c68c4ed2379f7c62eb4c0dabde81a42f
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-04-24 19:06:44 +0300

    refactor: портит проект, чтобы прошли тесты хекслета v2

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,54 +1,54 @@
-//import org.junit.jupiter.api.Test;
-//import hexlet.code.schemas.stringschema.StringSchema;
-//import hexlet.code.validator.Validator;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class AppTest {
-//
-//    @Test
-//    void testStringSchema() {
-//        Validator v = new Validator();
-//        StringSchema schema = v.string();
-//
-//        boolean expected = true;
-//        boolean actual = schema.isValid(""); // true
-//        assertEquals(expected, actual);
-//
-//        expected = true;
-//        actual = schema.isValid(null); // true
-//        assertEquals(expected, actual);
-//
-//        schema.required();
-//
-//        expected = true;
-//        actual = schema.isValid("what does the fox say"); // true
-//        assertEquals(expected, actual);
-//
-//        expected = true;
-//        actual = schema.isValid("hexlet"); // true
-//        assertEquals(expected, actual);
-//
-//        expected = false;
-//        actual = schema.isValid(null); // false
-//        assertEquals(expected, actual);
-//
-//        expected = false;
-//        actual = schema.isValid(""); // false
-//        assertEquals(expected, actual);
-//
-//        expected = true;
-//        actual = schema.contains("what").isValid("what does the fox say"); // true
-//        assertEquals(expected, actual);
-//
-//        expected = false;
-//        actual = schema.contains("whatthe").isValid("what does the fox say"); // false
-//        assertEquals(expected, actual);
-//
-//        expected = false;
-//        actual = schema.isValid("what does the fox say"); // false
-//        // уже false, так как добавлена ещё одна проверка contains("whatthe")
-//        assertEquals(expected, actual);
-//    }
-//
-//}
+import org.junit.jupiter.api.Test;
+import hexlet.code.schemas.stringschema.StringSchema;
+import hexlet.code.validator.Validator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testStringSchema() {
+        Validator v = new Validator();
+        StringSchema schema = v.string();
+
+        boolean expected = true;
+        boolean actual = schema.isValid(""); // true
+        assertEquals(expected, actual);
+
+        expected = true;
+        actual = schema.isValid(null); // true
+        assertEquals(expected, actual);
+
+        schema.required();
+
+        expected = true;
+        actual = schema.isValid("what does the fox say"); // true
+        assertEquals(expected, actual);
+
+        expected = true;
+        actual = schema.isValid("hexlet"); // true
+        assertEquals(expected, actual);
+
+        expected = false;
+        actual = schema.isValid(null); // false
+        assertEquals(expected, actual);
+
+        expected = false;
+        actual = schema.isValid(""); // false
+        assertEquals(expected, actual);
+
+        expected = true;
+        actual = schema.contains("what").isValid("what does the fox say"); // true
+        assertEquals(expected, actual);
+
+        expected = false;
+        actual = schema.contains("whatthe").isValid("what does the fox say"); // false
+        assertEquals(expected, actual);
+
+        expected = false;
+        actual = schema.isValid("what does the fox say"); // false
+        // уже false, так как добавлена ещё одна проверка contains("whatthe")
+        assertEquals(expected, actual);
+    }
+
+}
